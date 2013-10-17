5699e38008e63a47c8e9f18e24101962d87106b8
profesor <profesor@escom-OptiPlex-980.(none)>
2013-10-17 14:48:40 -0500

    abcd

---
+++ servidor.py
@@ -0,0 +1,22 @@
+#encoding:utf-8
+
+#Repositorio de Daniel Ramos
+import socket  
+  
+s = socket.socket()   
+s.bind(("localhost", 8000))  
+s.listen(1)  
+  
+sc, addr = s.accept()  
+  
+while True:  
+      recibido = sc.recv(1024)  
+      if recibido == "salir":  
+         break        
+      print "Recibido:", recibido  
+      sc.send(recibido)  
+  
+print "adios"  
+  
+sc.close()  
+s.close() 
